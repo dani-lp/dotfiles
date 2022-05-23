fcab2c29194a9a566e9c68b9450856805382f6ae
dani-lp <danilau11@gmail.com>
2022-05-23 10:26:37 +0200

    [FISH] Dots loading function

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,12 @@
 set -g -x fish_greeting ''
 
 source ~/.config/fish/.shells/alias
-source ~/.config/fish/.shells/functions
-source ~/.config/fish/.shells/exports
+# source ~/.config/fish/.shells/functions
+# source ~/.config/fish/.shells/exports
 
 # functions TODO move to external files
-function reload_dots
-  echo "Reloading dots"
+function save_dots
+  echo "Saving dots to repo"
 
   echo "Copying bat config..."
   /bin/cp -rf ~/.config/bat/ ~/dev/dotfiles/.config/
@@ -31,6 +31,34 @@ function reload_dots
 
   echo "Copying starship config..."
   /bin/cp -rf ~/.config/starship.toml ~/dev/dotfiles/.config/
+end
+
+function load_dots
+  echo "Loading dots into system"
+
+  echo "Loading bat config..."
+  /bin/cp -rf ~/dev/dotfiles/.config/bat/ ~/.config/
+
+  echo "Loading fish config..."
+  /bin/cp -rf ~/dev/dotfiles/.config/fish/ ~/.config/
+
+  echo "Loading kitty config..."
+  /bin/cp -rf ~/dev/dotfiles/.config/kitty/ ~/.config/
+
+  echo "Loading neofetch config..."
+  /bin/cp -rf ~/dev/dotfiles/.config/neofetch/ ~/.config/
+
+  echo "Loading neovim config..."
+  /bin/cp -rf ~/dev/dotfiles/.config/nvim/ ~/.config/
+
+  echo "Loading qtile config..."
+  /bin/cp -rf ~/dev/dotfiles/.config/qtile/ ~/.config/
+
+  echo "Loading rofi config..."
+  /bin/cp -rf ~/dev/dotfiles/.config/rofi/ ~/.config/
+
+  echo "Loading starship config..."
+  /bin/cp -rf ~/dev/dotfiles/.config/starship.toml ~/.config/
 end
 
 # exports
