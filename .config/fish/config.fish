0743dc8808053bd1478342f2822bcc0e744efea3
dani-lp <danilau11@gmail.com>
2022-07-02 18:00:00 +0200

    [FISH, KITTY] Random terminal qol improvements

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,7 @@ set -g -x fish_greeting ''
 source ~/.config/fish/alias
 
 # exports
-set PATH $PATH $HOME/.local/Aseprite $HOME/.cargo/bin
+set PATH $PATH $HOME/.local/Aseprite $HOME/.cargo/bin $HOME/.local/bin
 
 # init starship prompt
 starship init fish | source
