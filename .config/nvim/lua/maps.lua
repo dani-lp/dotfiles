30be45bb1597e23105f93d8f592e031cababb067
dani-lp <danilau11@gmail.com>
2022-09-19 21:56:26 +0200

    [NVIM] Added vs-code like single line movement

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -2,3 +2,5 @@ local keymap = vim.keymap
 
 -- TODO choose keymaps
 keymap.set('n', '<C-n>', '<cmd> NvimTreeToggle <CR>')
+keymap.set('n', '<C-S-j>', '<cmd> move+1 <CR>')
+keymap.set('n', '<C-S-k>', '<cmd> move-2 <CR>')
