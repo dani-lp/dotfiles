c887ff3d3a1483f24a657bd3ed3b3e599fd74c40
fabestah <101978594+fabestah@users.noreply.github.com>
2023-06-13 00:22:56 +0200

    [Qtile] Remove deprecated import functionality

--- .config/qtile/utils/variables.py
+++ .config/qtile/utils/variables.py
@@ -14,17 +14,6 @@ variables = {
     'two_monitors': False,
 }
 
-try:
-    with open(directory, 'r') as file:
-        config = json.load(file)
-        file.close()
-except FileNotFoundError:
-    print("Config file not found. Using default config")
-    with open(directory, 'w') as file:
-        file.write(json.dumps(variables, indent = 2))
-        config = variables.copy()
-        file.close()
-
 
 def load_settings(cls):
     def wrap():
